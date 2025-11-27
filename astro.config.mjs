20162093f3e57e150ce8aba02337544b1b07d3af
Chris Hartwig <chris@chris-hartwig.com>
2025-11-27 10:25:53 +0700

    adding sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://chris-hartwig.com',
   output: 'static',
   integrations: [
     react(),
     mdx(),
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    }),
   ],
 });
